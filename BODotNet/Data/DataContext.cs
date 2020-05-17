77744303aa328c8df2f0a5c9c5647f552739d93d
tarcisiocorte <tarcisiocorte@gmail.com>
2020-05-16 22:44:53 -0300

    DataContext

---
+++ BODotNet/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using BODotNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BODotNet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
