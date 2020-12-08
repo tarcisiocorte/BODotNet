135b867154849cf74c901e02e3cb6f95fc7b0e5b
Tarcisio Costa <tarcisiocorte@gmail.com>
2020-12-07 21:32:18 -0300

    entities as the first diagram

---
+++ BODotNet.Domain/Entities/AppRole.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Tccp.Domain.Entities
+{
+    public class AppRole : IdentityRole<int>
+    {
+        public ICollection<AppUserRole> UserRoles { get; set; }
+    }
+}
