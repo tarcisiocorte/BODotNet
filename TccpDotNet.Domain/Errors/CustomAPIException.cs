7172b767544a5d899530914ec4d16007b2536cdb
Tarcisio Costa <tarcisiocorte@gmail.com>
2020-12-10 08:50:33 -0300

    add custom CustomAPIExceptions

---
+++ TccpDotNet.Domain/Errors/CustomAPIException.cs
@@ -0,0 +1,16 @@
+﻿namespace Tccp.Domain.Errors
+{
+    public class CustomAPIException
+    {
+        public CustomAPIException(int statusCode, string message = null, string details = null)
+        {
+            this.statusCode = statusCode;
+            this.message = message;
+            this.details = details;
+        }
+
+        public int statusCode { get; set; }
+        public string message { get; set; }
+        public string details { get; set; }
+    }
+}
